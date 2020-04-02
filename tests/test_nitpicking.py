5881afab76cb5d3bc592f02bf8e3102a22646fb8
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-04-02 16:11:02 +0100

    Fix .vscode sometimes being tracked inside private
    This happens mostly due to https://github.com/vscode-restructuredtext/vscode-restructuredtext/issues/177
    
    @Tecnativa TT20357

--- tests/test_nitpicking.py
+++ tests/test_nitpicking.py
@@ -1,6 +1,10 @@
 """Nitpicking small tests ahead."""
+from pathlib import Path
+
 import pytest
 from copier.main import copy
+from plumbum import local
+from plumbum.cmd import git
 
 from .helpers import clone_self_dirty
 
@@ -12,8 +16,8 @@ WHITESPACE_PREFIXED_LICENSES = (
 
 
 @pytest.mark.parametrize("project_license", WHITESPACE_PREFIXED_LICENSES)
-def test_license_whitespace_prefix(tmpdir, project_license):
-    src, dst = tmpdir / "src", tmpdir / "dst"
+def test_license_whitespace_prefix(tmp_path: Path, project_license):
+    src, dst = tmp_path / "src", tmp_path / "dst"
     clone_self_dirty(src)
     copy(
         str(src),
@@ -22,4 +26,16 @@ def test_license_whitespace_prefix(tmpdir, project_license):
         force=True,
         data={"project_license": project_license},
     )
-    assert (dst / "LICENSE").read().startswith("   ")
+    assert (dst / "LICENSE").read_text().startswith("   ")
+
+
+def test_no_vscode_in_private(tmp_path: Path):
+    """Make sure .vscode folders are git-ignored in private folder."""
+    copy(".", str(tmp_path), vcs_ref="HEAD", force=True)
+    with local.cwd(tmp_path):
+        git("add", ".")
+        git("commit", "-am", "hello world")
+        vscode = tmp_path / "odoo" / "custom" / "src" / "private" / ".vscode"
+        vscode.mkdir()
+        (vscode / "something").touch()
+        assert not git("status", "--porcelain")
