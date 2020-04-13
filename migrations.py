322bae114e514e4653a4f4912d2b2e3711acdafc
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-04-13 10:25:26 +0100

    Modernize transition
    When using copier >= 3.0.5 (and you should use that), migrating from doodba-scaffolding skipped `.editorconfig` migration.
    
    Instead of fixing it in doodba-scaffolding, which is today archived, I prefer to fix it here.
    
    At the same time, the `scaffolding2copier` script gets the latest update.
    
    @Tecnativa TT20357 TT20739

--- migrations.py
+++ migrations.py
@@ -15,3 +15,10 @@ def from_doodba_scaffolding_to_copier(c):
     Path(".travis.yml").unlink()
     Path(".vscode", "doodbasetup.py").unlink()
     Path("odoo", "custom", "src", "private", ".empty").unlink()
+    # When using Copier >= 3.0.5, this file didn't get properly migrated
+    editorconfig_file = Path(".editorconfig")
+    editorconfig_contents = editorconfig_file.read_text()
+    editorconfig_contents = editorconfig_contents.replace(
+        "[*.yml]", "[*.{code-snippets,code-workspace,json,md,yaml,yml}{,.jinja}]", 1
+    )
+    editorconfig_file.write_text(editorconfig_contents)
