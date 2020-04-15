43de91c1409db39ae6cacf241f2d746efc873b0d
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-04-15 08:40:22 +0100

    Add all .docker to .gitignore
    Users before could use that to store more secret stuff. Let them be able to continue using it as such.

--- docs/scaffolding2copier.sh
+++ docs/scaffolding2copier.sh
@@ -12,7 +12,7 @@
 
 source .env
 copier $CUSTOM_COPIER_FLAGS \
-  -r "${TEMPLATE_VERSION-v1.4.1}" \
+  -r "${TEMPLATE_VERSION-v1.4.2}" \
   -d project_name="${PROJECT_NAME-$(basename $PWD)}" \
   -d project_license="${LICENSE-BSL-1.0}" \
   -d gitlab_url="${GITLAB_PREFIX-https://gitlab.com/example}/${PROJECT_NAME-$(basename $PWD)}" \
