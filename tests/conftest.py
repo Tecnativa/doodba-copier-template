c4f9451f070eb61956ddf27719d93290f9dc31ba
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-11-17 15:22:26 +0100

    [FIX] Change PRERELEASE_ODOO_VERSIONS to 17

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ SELECTED_ODOO_VERSIONS = (
     frozenset(map(float, os.environ.get("SELECTED_ODOO_VERSIONS", "").split()))
     or ALL_ODOO_VERSIONS
 )
-PRERELEASE_ODOO_VERSIONS = {16.0}
+PRERELEASE_ODOO_VERSIONS = {17.0}
 
 # Postgres versions
 ALL_PSQL_VERSIONS = tuple(COPIER_SETTINGS["postgres_version"]["choices"])
